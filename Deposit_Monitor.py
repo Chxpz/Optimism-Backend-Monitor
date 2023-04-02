19d75d83f8622ed3a530d622ea6e28b9e581ef8f
Chxpz <rpcweb3@gmail.com>
2023-04-02 16:46:57 -0300

    Retrieve OP ETH Total Supply

--- Deposit_Monitor.py
+++ Deposit_Monitor.py
@@ -1,11 +1,13 @@
 from Deposit_Monitor_L1 import monitor_deposits_L1
 from Deposit_Monitor_L2 import monitor_deposits_L2
+from Get_OVM_ETH_Total_Supply import OVM_ETH_Total_Supply
 import threading
 
 
 def main():
     threads = [threading.Thread(target=monitor_deposits_L1),
-               threading.Thread(target=monitor_deposits_L2)]
+               threading.Thread(target=monitor_deposits_L2),
+               threading.Thread(target=OVM_ETH_Total_Supply)]
     
     for t in threads:
         t.start()
---
+++ Get_OVM_ETH_Total_Supply.py
@@ -0,0 +1,44 @@
+from web3 import Web3
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import json
+import time
+
+def OVM_ETH_Total_Supply():
+    load_dotenv()
+    rpc_provider_URL = os.getenv('L2_RPC_PROVIDER_URL')
+    private_key = os.getenv('PRIVATE_KEY')
+    mongo_string = os.getenv('MONGO_STRING')
+
+    client = MongoClient(mongo_string)
+    db = client["OptimismLogs"]
+    collection = db["OVM_ETH_Total_Supply"]
+
+    abi_path_1 = os.path.abspath('./Abis/OVM_ETH.json')
+
+    with open(abi_path_1) as f:
+        Contract1Abi = json.load(f)
+
+    OVM_ETH_CONTRACT_ADDRESS = "0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000"
+    OVM_ETH_CONTRACT_ADDRESS_CHECKSUM = Web3.to_checksum_address(OVM_ETH_CONTRACT_ADDRESS)
+
+    w3 = Web3(Web3.HTTPProvider(rpc_provider_URL))
+
+    OVM_ETH = w3.eth.contract(address=OVM_ETH_CONTRACT_ADDRESS_CHECKSUM, abi=Contract1Abi)
+
+    while True:
+        total_supply = str(OVM_ETH.functions.totalSupply().call())
+        timestamp = int(time.time())
+        collection.insert_one({
+            "OP-ETH-Supply": total_supply,
+            "timestamp": timestamp
+        })
+        time.sleep(10)
+        print("OP-ETH-Supply: ", total_supply, "Timestamp: ", timestamp)
+
+if __name__ == '__main__':
+    try:
+        OVM_ETH_Total_Supply()
+    except Exception as e:
+        print(f"Error: {e}")
